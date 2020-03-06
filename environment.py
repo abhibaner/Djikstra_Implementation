1cbb9645f4062ccf8f1ec119b791397d9746fb4e
AbhishekB <abhibanerjee2494@gmail.com>
2020-03-06 00:31:04 -0500

    Updated obstacle space, along with dijkstra implementation

--- environment.py
+++ environment.py
@@ -1,13 +1,13 @@
-import numpy as np 
-import matplotlib.pyplot as plt
+import numpy as np
 import cv2
+from collections import deque
+import math
 
-
-class Nodes:    
-    def __init__(self,state,parent,cost):
+class Nodes:
+    def __init__(self,state):
         self.state = state
-        self.cost = cost
-        self.parent = parent
+        self.cost = 19999
+        self.parent = None
 
 def coll_circle(position):
 	p_x = position[1]
@@ -16,7 +16,7 @@ def coll_circle(position):
 	if location < 0:
 		return True
 	else:
-		return False 
+		return False
 
 def coll_ellipse(position):
 	p_x = position[1]
@@ -26,10 +26,10 @@ def coll_ellipse(position):
 	if location < 0:
 		return True
 	else:
-		return False 
+		return False
 
 def area(x1, y1, x2, y2, x3, y3):
-	return abs((x1 * (y2 - y3) + x2 * (y3 - y1)+ x3 * (y1 - y2)) / 2.0) 
+	return abs((x1 * (y2 - y3) + x2 * (y3 - y1)+ x3 * (y1 - y2)) / 2.0)
 
 def isInside(x1, y1, x2, y2, x3, y3, x, y):
 	A = area (x1, y1, x2, y2, x3, y3)
@@ -113,106 +113,254 @@ def coll_check(position):
 		return False
 
 def moveRight(position):
-	p_x = position[0]
-	p_y = position[1]
-	if p_x<300 and not (coll_check(position)):
-		updated_pos = [p_x+1,p_y]
+	p_x = position[1]
+	p_y = position[0]
+	if p_y<300 and not (coll_check((position[1],position[0]))):
+		updated_pos = [p_y+1,p_x]
 		cost = 1
+		#print(position,updated_pos)
 		return updated_pos,cost
 	else:
 		return None,None
 
 def moveLeft(position):
-	p_x = position[0]
-	p_y = position[1]
-	if p_x>0 and not (coll_check(position)):
-		updated_pos = [p_x-1,p_y]
+	p_x = position[1]
+	p_y = position[0]
+	if p_y>0 and not (coll_check((position[1],position[0]))):
+		updated_pos = [p_y-1,p_x]
 		cost = 1
 		return updated_pos,cost
 	else:
 		return None,None
 
 def moveUp(position):
-	p_x = position[0]
-	p_y = position[1]
-	if p_y<200 and not (coll_check(position)):
-		updated_pos = [p_x,p_y+1]
+	p_x = position[1]
+	p_y = position[0]
+	if p_x>0 and not (coll_check((position[1],position[0]))):
+		updated_pos = [p_y,p_x-1]
 		cost = 1
 		return updated_pos,cost
 	else:
 		return None,None
 
 def moveDown(position):
-	p_x = position[0]
-	p_y = position[1]
-	if p_y>0 and not (coll_check(position)):
-		updated_pos = [p_x,p_y-1]
+	p_x = position[1]
+	p_y = position[0]
+	if p_x<200 and not (coll_check((position[1],position[0]))):
+		updated_pos = [p_y,p_x+1]
 		cost = 1
 		return updated_pos,cost
 	else:
 		return None,None
 
 def moveUpRight(position):
-	p_x = position[0]
-	p_y = position[1]
-	if p_y<200 and p_x<300 and not (coll_check(position)):
-		updated_pos = [p_x+1,p_y+1]
+	p_x = position[1]
+	p_y = position[0]
+	if p_x>0 and p_y<300 and not (coll_check((position[1],position[0]))):
+		updated_pos = [p_y+1,p_x-1]
 		cost = math.sqrt(2)
 		return updated_pos,cost
 	else:
 		return None,None
 
 def moveDownRight(position):
-	p_x = position[0]
-	p_y = position[1]
-	if p_y>0 and p_x<300 and not (coll_check(position)):
-		updated_pos = [p_x,p_y-1]
+	p_x = position[1]
+	p_y = position[0]
+	if p_x<200 and p_y<300 and not (coll_check((position[1],position[0]))):
+		updated_pos = [p_y+1,p_x+1]
 		cost = math.sqrt(2)
 		return updated_pos,cost
 	else:
 		return None,None
 
 def moveUpLeft(position):
-	p_x = position[0]
-	p_y = position[1]
-	if p_y<200 and p_x>0 and not (coll_check(position)):
-		updated_pos = [p_x,p_y-1]
+	p_x = position[1]
+	p_y = position[0]
+	if p_x>0 and p_y>0 and not (coll_check((position[1],position[0]))):
+		updated_pos = [p_y-1,p_x-1]
 		cost = math.sqrt(2)
 		return updated_pos,cost
 	else:
 		return None,None
 
 def moveDownLeft(position):
-	p_x = position[0]
-	p_y = position[1]
-	if p_y>0 and p_x>0 and not (coll_check(position)):
-		updated_pos = [p_x,p_y-1]
+	p_x = position[1]
+	p_y = position[0]
+	if p_x<200 and p_y>0 and not (coll_check((position[1],position[0]))):
+		updated_pos = [p_y-1,p_x+1]
 		cost = math.sqrt(2)
 		return updated_pos,cost
 	else:
 		return None,None
 
-def main():
-	img = np.zeros((200,300,3), np.uint8)
+def mover(position,action):
+    if action == 'right':
+        [updated_pos,cost] = moveRight(position)
+    elif action == 'left':
+        [updated_pos,cost] = moveLeft(position)
+    elif action == 'up':
+        [updated_pos,cost] = moveUp(position)
+    elif action == 'down':
+        [updated_pos,cost] = moveDown(position)
+    elif action == 'upRight':
+        [updated_pos,cost] = moveUpRight(position)
+    elif action == 'upLeft':
+        [updated_pos,cost] = moveUpLeft(position)
+    elif action == 'downRight':
+        [updated_pos,cost] = moveDownRight(position)
+    elif action == 'downLeft':
+        [updated_pos,cost] = moveDownLeft(position)
+    return updated_pos,cost
 
-	BLUE = (255,0,0)
-	GREEN = (0,255,0)
-	
-	for i in range(200):
-		for j in range(300):
-			if coll_check([i,j]):
-				img[i][j] = BLUE
+def mappingofneighbors(current_position):
+    neighbors=['right','upRight','up','upLeft','left','downLeft','down','downRight']
+
+    active_points=[]
+    for action in neighbors:
+        new_pos,cost = mover(current_position,action)
+        active_points.append((new_pos,cost))
+    #active_points = [new_pos for new_pos in active_points if new_pos!= None]
+    return active_points
+'''
+def dijkstra(map,start,goal):
+    visited = {} #set of all points visited
+    visited.add(start_point)
+    nodes=[]
+    nodes.append(start_point)
+    Q=deque()
+    Q.append(0)
+    actual_costs=math.inf
+    cost2come=0
+
+    while Q:
+        parent = Q.popleft()
+        new_pos= nodes[parent]
+        neighbor_list= mappingofneighbors(new_pos)
+
+        for new in neighbors_list:
+            point = new[0]
+            cost  = new[1]
+            if point not in visited:
+                visited.add(point)
+                nodes.append(point)
+                Q.append(len(nodes)-1)
+            if cost2come + cost < actual_costs:
+                actual_costs= cost2come+cost
+            if point == goal:
+                break
+'''
+def priority_pop(queue):  # Priority Queue, outputs the node with least cost attached to it
+    min_a = 0
+    for elemt in range(len(queue)):
+        if queue[elemt].cost < queue[min_a].cost:
+            min_a = elemt
+    return queue.pop(min_a)
+
+def find_node(point, queue):
+    for elem in queue:
+        if elem.state == point:
+            return queue.index(elem)
+        else:
+            return None
+
+def track_back(node):
+    print("Tracking Back")
+    p = []
+    p.append(node.parent)
+    parent = node.parent
+    if parent is None:
+        return p
+    while parent is not None:
+        p.append(parent)
+        parent = parent.parent
+    return p
+
+def color_pixel(image_color, point):
+    #print(point)
+    image_color[point[1], point[0]] = [255,0,255]
+    return image_color
+
+def plan_algo(start,goal,imap):
+	visited = []
+	s_node = Nodes(start)
+	s_node.cost = 0
+	Q = [s_node]
+	imap[start[1], start[0]] = [255, 0, 0]
+	imap[goal[1], goal[0]] = [0, 0, 255]
+	while Q:
+		current = priority_pop(Q)
+		visited.append(current.state)
+		neighbors = mappingofneighbors(current.state)
+
+		for n in neighbors:
+			if n[0] is not None:
+
+				new_node = Nodes(n[0])
+				#print(n[0])
+				new_node.parent = current
+
+				if n[0][0]==goal[0] and n[0][1]==goal[1]:
+					print("Goal Reached")
+					return new_node,imap
+				#print(current.state)
+				#imap = color_pixel(imap, current.state)
+				#imap[start[1], start[0]] = [255, 0, 0]
+				#imap[goal[1], goal[0]] = [0, 0, 255]
+
+				#resized_new_1 = cv2.resize(imap, None, fx=4, fy=4, interpolation=cv2.INTER_CUBIC)
+				#cv2.imshow("Figure", resized_new_1)
+				#cv2.waitKey(1)
+
+				if n[0] not in visited:
+					#print("Here's Johnny")
+					new_node.cost = n[1]+new_node.parent.cost
+					visited.append(new_node.state)
+					Q.append(new_node)
+				else:
+					node_id = find_node(new_node,Q)
+
+					if node_id:
+						t_node = queue[node_id]
+						if t_node.cost> n[1]+new_node.parent.cost:
+							t_node.cost= n[1]+new_node.parent.cost
+							t_node.parent = current
 			else:
-				img[i][j] = GREEN
-			
+				continue
 
-	plt.imshow(img)
-	plt.show()
+	return None,None
+
+
+
+
+
+def main():
+	img = np.zeros((201,301,3), np.uint8)
+
+	BLACK = (0,0,0)
+	WHITE = (255,255,255)
+
+	for i in range(201):
+		for j in range(301):
+			if coll_check([i,j]):
+				img[i][j] = BLACK
+			else:
+				img[i][j] = WHITE
+
+
+	start = [5,195]
+	goal = [295,5]
+	result,image = plan_algo(start,goal,img)
+	n_list = track_back(result)
+	for elem in n_list:
+		x = elem.state[1]
+		y = elem.state[0]
+		image[x, y] = [0, 255, 0]
+		resized_new = cv2.resize(image, None, fx=4, fy=4, interpolation=cv2.INTER_CUBIC)
+		cv2.imshow("Figure", resized_new)
+		cv2.waitKey(100)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 	#cv2.imshow('Environment',img)
 	#cv2.waitKey(0)
-
-
 if __name__ == '__main__':
-    main() 
-
-
+    main()
